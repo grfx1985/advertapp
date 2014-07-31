138f35f59003c89a83fd9114c73f5ad4e5d696cb
grfx85 <michalandros@gmail.com>
2014-07-31 12:13:33 +0200

    added pagination count to 5 per page and changed the style to applestore like

--- app/controllers/adverts_controller.rb
+++ app/controllers/adverts_controller.rb
@@ -4,7 +4,7 @@ class AdvertsController < ApplicationController
   # GET /adverts
   # GET /adverts.json
   def index
-    @adverts = Advert.all
+    @adverts = Advert.paginate(:page => params[:page], :per_page => 5)
   end
 
   # GET /adverts/1
